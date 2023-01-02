330a9b2029306a3b6ba6ef02dcd048b557df0d50
Arjun <112770763+Tango404@users.noreply.github.com>
2023-01-02 00:14:16 -0500

    Version 1 [Inital Release]
    Uploaded game files to repo

---
+++ 2D-Game/DO NOT TOUCH/game.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MULTIPLE5 5
+
+#define MAX_LIVES 10
+#define MIN_LIVES 1
+
+#define MAX_MOVES 15
+#define MIN_MOVES 3
+
+#define MAX_PATH_LENGTH 70
+#define MIN_PATH_LENGTH 10
+
+struct playerInfo
+{
+    int lives;
+    char symbol;
+    char history[MAX_PATH_LENGTH];
+    int treasureCounter;
+    int nextMove;
+
+};
+
+struct gameInfo
+{
+    int moves;
+    int pathLength;
+    int bombs[MAX_PATH_LENGTH];
+    int treasure[MAX_PATH_LENGTH];
+};
+
+int flag = 0; 
+int flag2 = 0;
+int i = 0;
+int j = 0;
